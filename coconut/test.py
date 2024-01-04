ca549d32532f8c3a6dddc4b223709b3aed366569
dw <1798827828@qq.com>
2024-01-04 15:45:24 -0500

    fix

---
+++ coconut/test.py
@@ -0,0 +1,21 @@
+import os
+from tqdm import tqdm
+
+os.environ['TRANSFORMERS_CACHE'] = '/datasets/Large_Language_Models'
+from datasets import load_dataset, load_from_disk
+
+dataset = load_dataset('data/apr_rlhf_coconut', split='train')
+
+
+def process(sample):
+    pre, suffix = sample['prompt'].split('<FILL_ME>')[0], sample.split('<FILL_ME>')[0]
+    sample['prompt'] = "<PRE> " + pre + " <SUF>" + suffix + " <MID>"
+    return sample
+
+
+dataset['train'] = dataset['train'].map(process)
+dataset['test'] = dataset['train'].map(process)
+
+dataset.save_to_disk(f"data/apr_rlhf_coconut")
+
+print(dataset['train'][0])['prompt']
