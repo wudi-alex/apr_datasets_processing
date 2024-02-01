504a68591623db00ce9f2b892f3dfbafcb766e82
alex.wu <18721601773@163.com>
2024-01-31 20:43:38 -0500

    fix

--- java_mutation/process.py
+++ java_mutation/process.py
@@ -17,8 +17,15 @@ def get_token_len(input):
     return len(input_tokens[0])
 
 
-dataset = dataset.filter(
-    lambda x: get_token_len(x['input']) < 1024, num_proc=40)
+def token_len(sample):
+    sample['input_len'] = get_token_len(sample['input'])
+    sample['output_len'] = get_token_len(sample['output'])
+    return sample
+
+
+dataset = dataset.map(token_len)
+dataset.save_to_disk('data/classinfo_mutation_full')
+print('token len done.')
 
 dataset = dataset.train_test_split(test_size=0.05)
 
