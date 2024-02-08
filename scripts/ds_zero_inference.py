30b22af3557a34534b82ab83747f0185352dbf3b
alex.wu <18721601773@163.com>
2024-02-08 01:57:26 -0500

    fix

--- scripts/ds_zero_inference.py
+++ scripts/ds_zero_inference.py
@@ -204,8 +204,9 @@ def run_generation(
                            num_return_sequences=num_outputs, early_stopping=early_stopping,
                            eos_token_id=tokenizer.eos_token_id,
                            pad_token_id=tokenizer.pad_token_id,
-                           top_k=50,  # 仅考虑概率最高的50个词
-                           top_p=0.95)
+                           # top_k=50,  # 仅考虑概率最高的50个词
+                           # top_p=0.95
+                           )
 
     def _batch_encode(inputs):
         input_tokens = tokenizer.batch_encode_plus(inputs, return_tensors="pt", padding="max_length",
