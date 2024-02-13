30090ca29338adf8d162071a9593c3c0f44f1781
alex.wu <18721601773@163.com>
2024-02-12 20:35:19 -0500

    fix

--- scripts/bash_scripts/run_classinfo_codellama.sh
+++ scripts/bash_scripts/run_classinfo_codellama.sh
@@ -1,3 +1,16 @@
+#!/bin/bash
+#SBATCH --partition=contrib-gpuq
+#SBATCH --qos=ksun
+#SBATCH --job-name=codellama_classinfo_finetune_lora
+#SBATCH --output=/projects/ksun3/%u/sbatch_log/%x-%N-%j.out
+#SBATCH --error=/projects/ksun3/%u/sbatch_log/%x-%N-%j.err
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:A100.80gb:4
+#SBATCH --ntasks-per-node=20
+#SBATCH --mem=480G
+#SBATCH --export=ALL
+#SBATCH --time=5-00:00:00
+
 accelerate launch llama_sft.py \
     --model_name_or_path codellama/CodeLlama-7b-hf \
     --data_path /projects/ksun3/dwu25/apr_datasets_processing/java_mutation/data/classinfo_mutation \
@@ -6,14 +19,14 @@ accelerate launch llama_sft.py \
     --do_train \
     --do_eval True \
     --fp16 True \
-    --output_dir /projects/ksun3/dwu25/trained_models/classinfo_mutation_output \
+    --output_dir /projects/ksun3/dwu25/trained_models/codellama_classinfo_finetune_lora \
     --num_train_epochs 1 \
     --per_device_train_batch_size 32 \
     --per_device_eval_batch_size 1 \
     --gradient_accumulation_steps 1 \
     --evaluation_strategy "no" \
     --eval_steps 1000 \
-    --save_steps 150 \
+    --save_steps 1000 \
     --learning_rate 5e-4 \
     --lr_scheduler_type "cosine" \
     --logging_steps 10 \
